fb2087f26580c8a8b22fccf56626d54913328647
João Marcus de Lemos Fernandes <jmpierrotfernandes@hotmail.com>
2016-04-01 17:42:24 -0300

    Mudado minify-css para cleanCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var minifycss = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', function() {
 	console.log("Watching:");
 });
- 
+
 gulp.task('sass', function () {
   return sass('sass/*.scss')
     .on('error', sass.logError)
     .pipe(gulp.dest('css/')
-    .pipe(minifycss()));
+    .pipe(cleanCSS({compatibility: 'ie8'})));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('sass/**/*.scss',['sass']);
 });
+
